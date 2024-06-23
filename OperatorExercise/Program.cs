36df7ce478945c6b09462f94d7ef05477605051e
Kevin Clark <kevinclarkengineer@gmail.com>
2024-06-23 17:41:28 -0400

    Exercises Complete :)

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -1,10 +1,35 @@
-﻿namespace OperatorExercise
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace OperatorExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int a = 17;
+            int b = 4;
+
+            int sum = a + b;
+            int sub = a - b;
+            int mult = b * a;
+
+            double quotient = a / b;
+            double remainder = a % b;
+
+            Console.WriteLine($"17 plus 4 = {sum}, 17 minus 4 = {sub}, 4 times 17 equals {quotient}. \n 17 divided by 4 equals {quotient}, 17 divided by 4 has a rounded remainder of {remainder}");
+
+            double radius = double.Parse(Console.ReadLine());
+
+            AreaOfCircle(radius);
+        }
+       static double AreaOfCircle(double radius)
+        {
+            double area = Math.PI * Math.Pow(radius, 2);
+            
+            Console.WriteLine($"The Area of the Circle with Radius {radius} is equal to: {area}");
+            return area;
         }
     }
+    
 }
+
